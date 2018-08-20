9b906cafb6c0e586a7780a50d1f1696ed1187380
Ben Rodia <ben@bstudiosonline.com>
2018-08-20 00:18:21 -0400

    Add palette swapping

--- src/php/gallery.php
+++ src/php/gallery.php
@@ -2,6 +2,8 @@
 
 <section class="gallery" id="gallery">
 
+
+
 		<div class="SVGbox">
 			<?php include 'img/htmlsvgs/An_Christina.html'; ?>	
 			<h2 class="artistTitle">Christina An</h2>	
@@ -26,7 +28,91 @@
 			<p></p>	
 		</div>	
 
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Hove_Travis.html'; ?>	
+			<h2 class="artistTitle">Travis Hove</h2>	
+			<p></p>	
+		</div>
+		
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Lewing_Maxime.html'; ?>	
+			<h2 class="artistTitle">Maxime Lewing</h2>	
+			<p></p>	
+		</div>
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Lynch_Fiona.html'; ?>	
+			<h2 class="artistTitle">Fiona Lynch</h2>
+			<p></p>	
+		</div>
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Meixner_Sara.html'; ?>	
+			<h2 class="artistTitle">Sara Meixner</h2>	
+			<p></p>	
+		</div>	
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/RizzutoSmith_Elijah.html'; ?>	
+			<h2 class="artistTitle">Elijah Rizzuto</h2>	
+			<p></p>	
+		</div>
+		
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Schultz_Julia.html'; ?>	
+			<h2 class="artistTitle">Julia Schultz</h2>	
+			<p></p>	
+		</div>
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Spurr_Benn.html'; ?>	
+			<h2 class="artistTitle">Benn Spurr</h2>
+			<p></p>	
+		</div>
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Srnka_Anthony.html'; ?>	
+			<h2 class="artistTitle">Anthony Srnka</h2>	
+			<p></p>	
+		</div>	
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Tercek_Clay.html'; ?>	
+			<h2 class="artistTitle">Clay Tercek</h2>	
+			<p></p>	
+		</div>
+		
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Toone_Michael.html'; ?>	
+			<h2 class="artistTitle">Michael Toone</h2>	
+			<p></p>	
+		</div>
+
+		<div class="SVGbox">
+			<?php include 'img/htmlsvgs/Wagner_Ciaran.html'; ?>	
+			<h2 class="artistTitle">Ciaran Wagner</h2>
+			<p></p>	
+		</div>
+
+
+<?php 
+
+
+// $json = include 'svg/coloring_img.json';
+
+// $images = json_decode($json);
+
+// //looping through images in the json file
+// foreach($images as $img){
+//    echo '<div class="SVGbox">';
+//    echo $img[‘author’];
+//    echo '</div>';
+//    }
+
+
+ ?>
 	<div id="canvas" onmousemove="showCoords(event)">
 	</div>
 </section>
+
 
